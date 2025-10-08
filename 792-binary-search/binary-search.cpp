2ddeb47d9a30ea52312e5b8b3b570a3e6953c4d0
Hardika Khare <144809410+HardikaKhare@users.noreply.github.com>
2025-10-08 11:57:32 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (9.33%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -4,7 +4,6 @@ public:
         int n=nums.size();
         int low=0;
         int high=n-1;
-        
         while(low<=high){
             int mid=(low+high)/2;
             if(nums[mid]==target){
