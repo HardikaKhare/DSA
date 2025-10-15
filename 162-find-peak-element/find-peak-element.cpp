9dc6978ea321b05650b03532342990f9c0c215c3
Hardika Khare <144809410+HardikaKhare@users.noreply.github.com>
2025-10-15 19:09:15 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (92.79%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -11,7 +11,7 @@ public:
             int mid=(low+high)/2;
             if(nums[mid]>nums[mid-1]&& nums[mid]>nums[mid+1]){
                 return mid;
-            }else if(nums[mid]<nums[mid+1]){
+            }else if(nums[mid]>nums[mid-1]){
                 low=mid+1;
             }else{
                 high=mid-1;
