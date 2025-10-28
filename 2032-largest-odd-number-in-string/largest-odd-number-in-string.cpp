252ddba35ec2732e784a25a05f532f89451d4f97
Hardika Khare <144809410+HardikaKhare@users.noreply.github.com>
2025-10-28 19:32:42 +0530

    Time: 1 ms (41.89%) | Memory: 17.9 MB (36.34%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -6,7 +6,6 @@ public:
         for(int i=n-1;i>=0;i--){
            if((num[i]-'0') %2 ==1){
                 return num.substr(0,i+1);
-                break;
            }
        }
         return "";
