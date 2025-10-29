2f15f3ba78ca14f0ca42c6164568f6aa0aa072d8
Hardika Khare <144809410+HardikaKhare@users.noreply.github.com>
2025-10-29 20:25:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (36.07%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -3,14 +3,7 @@ public:
     bool rotateString(string s, string goal) {
         int n=s.size();
          if (n != goal.size()) return false;
-       for (int i = 0; i < n; i++) {
-            // rotate s by one position each time
-            reverse(s.begin(), s.begin() + 1);
-            reverse(s.begin() + 1, s.end());
-            reverse(s.begin(), s.end());
-            
-            if (s == goal) return true;
-       }
-        return false;
+        string doubledS = s + s;
+        return doubledS.find(goal) != string::npos;
     }
 };
