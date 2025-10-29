fde21e9fadf5720ffd0ac03fdb0a16562d801a73
Hardika Khare <144809410+HardikaKhare@users.noreply.github.com>
2025-10-29 21:51:05 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (91.17%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t) return true;
-        else return false;
+        int hash[26]={0};
+        int n=s.size();
+        if (s.size() != t.size()) return false; 
+        for(int i=0;i<n;i++){
+            hash[s[i]-'a']++;
+        }
+        for(int i=0;i<n;i++){
+            hash[t[i]-'a']--;
+        }
+        for(int i=0;i<26;i++){
+             if (hash[i] != 0) return false;
+        }
+        return true;
     }
 };
