47f72f052d33ab9d79a5d9618d1a23b2dae327fd
Hardika Khare <144809410+HardikaKhare@users.noreply.github.com>
2025-11-03 20:26:31 +0530

    Time: 10 ms (32.51%) | Memory: 12.2 MB (97.48%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
@@ -9,9 +9,9 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-         node->val = node->next->val;      // Step 1: copy next node's value
-    ListNode* temp = node->next;      // Step 2: store next node
-    node->next = node->next->next;    // Step 3: skip the next node
+         node->val = node->next->val;     
+    ListNode* temp = node->next;    
+    node->next = node->next->next;    
     delete temp;  
     }
 };
