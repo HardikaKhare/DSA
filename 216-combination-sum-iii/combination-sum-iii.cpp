abfa6319bbe17790cee3dce49de4f47da43412c0
Hardika Khare <144809410+HardikaKhare@users.noreply.github.com>
2026-02-02 16:04:12 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (12.13%) - LeetSync

---
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void subset(vector<vector<int>>&ans,int k, int n,vector<int>val,int sum,int idx){
+        if(val.size()==k && sum==n){
+            ans.push_back(val);
+            return;
+        }
+        if(sum>n) return;
+        for(int i=idx;i<=9;i++){
+            val.push_back(i);
+            subset(ans,k,n,val,sum+i,i+1);
+            val.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>>ans;
+        vector<int>val;
+        subset(ans,k,n,val,0,1);
+        return ans;
+    }
+};
