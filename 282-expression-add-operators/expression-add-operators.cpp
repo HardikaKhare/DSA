b3b70794cc0ad5354794222476fb0b188e4f1bbb
Hardika Khare <144809410+HardikaKhare@users.noreply.github.com>
2026-02-07 21:38:30 +0530

    Time: 359 ms (87.09%) | Memory: 92.2 MB (84.08%) - LeetSync

---
+++ 282-expression-add-operators/expression-add-operators.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+   void recurse(int idx,long long total,long long mult,vector<string>& ans,string s,string& num,int target) {
+        if(idx == num.size()){
+            if(total == target) ans.push_back(s);
+            return;
+        }
+        long long curr = 0;
+        for(int i = idx; i < num.size(); i++){
+            if(i > idx && num[idx] == '0') break;
+            curr = curr * 10 + (num[i] - '0');
+            string part = num.substr(idx, i - idx + 1);
+            if(idx == 0){
+                recurse(i+1, curr, curr, ans, part, num, target);
+            }
+            else{
+                recurse(i+1, total + curr, curr,ans, s + "+" + part, num, target);
+                recurse(i+1, total - curr, -curr,ans, s + "-" + part, num, target);
+                recurse(i+1,total - mult + mult*curr,mult*curr,ans, s + "*" + part, num, target);
+            }
+        }
+    }
+    vector<string> addOperators(string num, int target) {
+        vector<string> ans;
+        string s = "";
+        recurse(0, 0, 0, ans, s, num, target);
+        return ans;
+    }
+};
