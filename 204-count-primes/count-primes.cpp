95e7c996ac376166c8ee25f6c87797254b01eb49
Hardika Khare <144809410+HardikaKhare@users.noreply.github.com>
2026-03-18 22:00:40 +0530

    Time: 151 ms (88.54%) | Memory: 132.8 MB (8.49%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -3,6 +3,8 @@ public:
     int countPrimes(int n) {
         int cnt=0;
         vector<int>hash(n,1);
+        if(n > 0) hash[0] = 0;
+        if(n > 1) hash[1] = 0;
         for(int i = 2; i*i< n; i++) {
             if(hash[i]==1){
                 for(int j=i*i;j<n;j+=i){
